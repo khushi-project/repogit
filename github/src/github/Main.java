a69ef16fb77762537d61737eb08e6497002619f2
LOQ <LOQ@LAPTOP-JIPCLQJS>
2024-07-26 19:49:03 +0530

    update2

--- github/src/github/Main.java
+++ github/src/github/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("first code");
-		System.out.println("update 1");
+		System.out.println("first");
+		System.out.println("update 2");
 	}
 
 }
